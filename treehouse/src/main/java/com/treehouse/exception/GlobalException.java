962169cbe18839030cea028517c57bd689779fa9
Gaurav <patilgc83@gmail.com>
2022-12-17 14:56:30 +0530

    ExceptionAdded

--- treehouse/src/main/java/com/treehouse/exception/GlobalException.java
+++ treehouse/src/main/java/com/treehouse/exception/GlobalException.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.treehouse.model.MyErrorDetails;
@@ -37,6 +38,8 @@ public class GlobalException {
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 
 	}
+	
+	
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<MyErrorDetails> myExceptionHandler(Exception ie, WebRequest rq) {
@@ -59,5 +62,19 @@ public class GlobalException {
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 
 	}
+	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<MyErrorDetails> myExceptionHandler(MethodArgumentTypeMismatchException nhf, WebRequest rq) {
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimestamp(LocalDateTime.now());
+		err.setMessage(nhf.getMessage());
+		err.setDetails(rq.getDescription(false));
+
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+
+	}
+	
+	
+	
 
 }
